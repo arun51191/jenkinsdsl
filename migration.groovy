2fc6b87d504f548f0510bdec429706a2ff22cd28
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-30 13:08:57 +0100

    Update migration.groovy

--- migration.groovy
+++ migration.groovy
@@ -5,9 +5,7 @@ def database = System.getenv("Database")
 print "\nSource database = ${src}\n\n"
 print "Destination database = ${dest}\n\n"
 print "Datbase name = ${database}\n\n"
-//if (${src}==${dest}) {
-  print("Aborting the job as source and target databases are same")
-  def thr = Thread.currentThread()
-  def build = thr?.executable
-  build.interrupt();
-//}
+if (${src}==${dest}) {
+   currentBuild.result = 'SUCCESS'
+   return
+}
