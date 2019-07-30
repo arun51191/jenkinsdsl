4e819c0334be0ac1b640b8025831e09078c841be
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-30 07:38:51 +0100

    Create migration.groovy

---
+++ migration.groovy
@@ -0,0 +1,6 @@
+def src = System.getenv("S_ENV")
+def dest   = System.getenv("D_ENV")
+def database = System.getenv("Database")                
+print "\nSource database = ${src}\n\n"
+print "Destination database = ${dest}\n\n"
+print "Datbase name = ${database}\n\n"
