a3acda2b954817fc5162d181722cfa5529357d36
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-30 10:49:46 +0100

    Update migration.groovy

--- migration.groovy
+++ migration.groovy
@@ -1,9 +1,11 @@
-import hudson.AbortException
 def src = System.getenv("S_ENV")
 def dest   = System.getenv("D_ENV")
 def database = System.getenv("Database")                
 print "\nSource database = ${src}\n\n"
 print "Destination database = ${dest}\n\n"
 print "Datbase name = ${database}\n\n"
-assert ${src}==${dest} : "Build failed because of the source and target databases shouldn't be same"
-
+if( ${src} == ${dest} ) {
+   print "\n\n. Please choose different source and target environments"
+   currentBuild.result = 'SUCCESS'
+   return
+}
