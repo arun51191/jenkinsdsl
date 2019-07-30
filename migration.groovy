ba32b472ce2817ab4d67ae4c23d74f3410093e71
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-30 15:48:08 +0100

    Update migration.groovy

--- migration.groovy
+++ migration.groovy
@@ -1,11 +1,13 @@
 import hudson.model.*
-def src = System.getenv("S_ENV")
-def dest   = System.getenv("D_ENV")
-def database = System.getenv("Database")                
+import jenkins.model.CauseOfInterruption
+def src = build.buildVariableResolver.resolve("S_ENV")
+def dest   = build.buildVariableResolver.resolve("D_ENV")
+def database = build.buildVariableResolver.resolve("Database")                
 print "\nSource database = ${src}\n\n"
 print "Destination database = ${dest}\n\n"
 print "Datbase name = ${database}\n\n"
-if (${src}==${dest}) {
-   currentBuild.result = 'SUCCESS'
-   return
+if (src==dest) {
+    def exec = build.getExecutor()
+    def cause = { "source and target dbs are same" } as CauseOfInterruption 
+    exec.interrupt(Result.ABORTED, cause)
 }
