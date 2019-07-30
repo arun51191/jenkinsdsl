f66b53e51a13c0c94b29abe8132e71a88a00e9c1
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-30 12:13:06 +0100

    Update migration.groovy

--- migration.groovy
+++ migration.groovy
@@ -1,13 +1,13 @@
 import hudson.model.*
-import hudson.AbortException
-import hudson.console.HyperlinkNote
-import java.util.concurrent.CancellationException
 def src = System.getenv("S_ENV")
 def dest   = System.getenv("D_ENV")
 def database = System.getenv("Database")                
 print "\nSource database = ${src}\n\n"
 print "Destination database = ${dest}\n\n"
 print "Datbase name = ${database}\n\n"
-def autoCancelled = false
-
-assert ${src}==${dest} : "Source and target should be different"
+if (${src}==${dest}) {
+  print("Aborting the job as source and target databases are same")
+  def thr = Thread.currentThread()
+  def build = thr?.executable
+  build.interrupt();
+}
