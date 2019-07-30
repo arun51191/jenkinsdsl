ab7bbd8cbf02b5689220bd04bad01bc346885929
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-30 11:27:56 +0100

    Update migration.groovy

--- migration.groovy
+++ migration.groovy
@@ -7,18 +7,4 @@ print "Destination database = ${dest}\n\n"
 print "Datbase name = ${database}\n\n"
 def autoCancelled = false
 
-try {
-    if (${src}==${dest}) {
-      autoCancelled = true
-      error('Aborting the build.')
-    }
-  }
- catch (e) {
-  if (autoCancelled) {
-    currentBuild.result = 'SUCCESS'
-    // return here instead of throwing error to keep the build "green"
-    return
-  }
-  // normal error handling
-  throw e
-}
+assert ${src}==${dest} : "Source and target should be different"
