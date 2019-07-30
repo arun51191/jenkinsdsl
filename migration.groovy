0425648243e2a88e405ed5b40caf7aed72fa150f
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-30 08:20:28 +0100

    Update migration.groovy

--- migration.groovy
+++ migration.groovy
@@ -4,3 +4,6 @@ def database = System.getenv("Database")
 print "\nSource database = ${src}\n\n"
 print "Destination database = ${dest}\n\n"
 print "Datbase name = ${database}\n\n"
+if(${src}==${dest}){
+  error('Source and Destination environments should not be same')
+}
