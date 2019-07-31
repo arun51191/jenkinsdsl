a496b71b84e0a9dd4f3a4eb2e62c9fdcf730f4a4
arun51191 <34202644+arun51191@users.noreply.github.com>
2019-07-31 04:23:47 +0100

    Update migration.groovy

--- migration.groovy
+++ migration.groovy
@@ -11,4 +11,39 @@ if (src==dest||database=="") {
     def cause = { "source and target dbs are same or given empty database" } as CauseOfInterruption 
     exec.interrupt(Result.ABORTED, cause)
 }
-print "Build is completed"
+
+def env = System.getenv()
+def credentials = "RADEV"
+def source = "dev-hostname"
+def target   = "dev-hostname"
+
+if (src=="uat")
+{
+    credentials = "RAUAT"
+    source      = "uat-hostname"
+    
+}
+
+if (dest=="uat")
+{
+    credentials = "RAUAT"
+    target      = "uat-hostname"
+    
+}
+
+def pa1 = new ParametersAction([
+  new StringParameterValue("env", credentials)
+])
+
+def pa2 = new ParametersAction([
+  new StringParameterValue("env", source)
+])
+
+def pa3 = new ParametersAction([
+  new StringParameterValue("env", target)
+])
+
+// add variable to current job
+Thread.currentThread().executable.addAction(pa1)
+Thread.currentThread().executable.addAction(pa2)
+Thread.currentThread().executable.addAction(pa3)
